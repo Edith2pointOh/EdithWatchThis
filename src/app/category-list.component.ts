25af4395315877d2166206e8e2d6310a8abdbbf8
Edith Werbel <edith.werbel@gmail.com>
2020-11-11 16:15:54 -0500

    inital commit

---
+++ src/app/category-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "mw-category-list",
+  template: `
+    <span class="label" *ngFor="let category of categories">
+      {{ category }}
+    </span>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        margin-bottom: 20px;
+      }
+      span {
+        display: inline-block;
+        margin-right: 4px;
+        margin-bottom: 4px;
+      }.label {
+        background-color: #999999;
+        color: #ffffff;
+        border-radius: 4px;
+        padding: 2px 6px;
+        font-style: italic;
+        width: max-content;
+      }
+      
+    `
+  ]
+})
+export class CategoryListComponent {
+  @Input() categories: string[];
+}
